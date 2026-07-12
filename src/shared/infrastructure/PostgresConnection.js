2971b51d007d3c7d71587147f176522922d8abb8
Jhandel <zavaletajhandel@gmail.com>
2026-07-12 00:19:11 -0500

    chore: configurar PostgreSQL para producción

--- src/shared/infrastructure/PostgresConnection.js
+++ src/shared/infrastructure/PostgresConnection.js
@@ -1,7 +1,16 @@
 const { Pool } = require('pg');
 
+const isProduction =
+    process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+
+    ssl: isProduction
+        ? {
+              rejectUnauthorized: false
+          }
+        : false
 });
 
 pool.on('error', (error) => {
